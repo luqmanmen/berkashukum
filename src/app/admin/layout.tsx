d7e2d5237ffebb589c4334947e324407179d7179
Luqman Arif <luqmanarifsby@gmail.com>
2026-07-24 16:05:20 +0700

    fix: remove layout session redirect to fix login infinite loop

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,7 +11,7 @@ export default async function AdminLayout({
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect("/admin/login");
+    return <>{children}</>;
   }
 
   const navItems = [
