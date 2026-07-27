259e08cbe29906995698d389d1cbdde28eaeb177
Luqman Arif <luqmanarifsby@gmail.com>
2026-07-28 01:35:31 +0700

    fix: change bcrypt to bcryptjs in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../src/generated/prisma'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
